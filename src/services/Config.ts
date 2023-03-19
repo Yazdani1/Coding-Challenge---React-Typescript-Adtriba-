85284146e0062c63c2975d5fbd41dff8265b7115
Yazdani Chowdhury <yaz4dani@gmail.com>
2023-03-19 17:43:39 +0200

    Implement Pagination and Search Filter

--- src/services/Config.ts
+++ src/services/Config.ts
@@ -1,3 +1,3 @@
-// export const API_URL ="http://localhost:8080/api/v0"
+export const API_URL ="http://localhost:8080/api/v0"
 
-export const API_URL ="https://coding-challenge-node-js-server-adtriba.vercel.app/api/v0"
+// export const API_URL ="https://coding-challenge-node-js-server-adtriba.vercel.app/api/v0"
